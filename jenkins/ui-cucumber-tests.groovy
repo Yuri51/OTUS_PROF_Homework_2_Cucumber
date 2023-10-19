3da3a2803ed230d4cb056916519736cbe9d1a88f
Yuri <ananievyuri@mail.ru>
2023-10-19 23:57:04 +0300

    3

--- jenkins/ui-cucumber-tests.groovy
+++ jenkins/ui-cucumber-tests.groovy
@@ -12,22 +12,22 @@ timeout(180) {
             }
             stage('Run tests') {
                 tests_exit_code = sh(
-                        script: "mvn test -DbaseUrl=$BASE_URL -Dbrowser=$BROWSER",
+                        script: "mvn test -DbaseUrl=$BASE_URL -Dbrowser=$BROWSER" -Dbrowser.version=${BROWSER_VERSION},
                         returnStatus: true,
                 )
                 if (tests_exit_code != 0) {
                     currentBuild.result = 'UNSTABLE'
                 }
             }
-            stage('Publish artifacts') {
-                allure([
-                        includeProperties: false,
-                        jdk              : '',
-                        properties       : [],
-                        reportBuildPolicy: 'ALWAYS',
-                        results          : [[path: 'build/reports/allure-results']]
-                ])
-            }
+//            stage('Publish artifacts') {
+//                allure([
+//                        includeProperties: false,
+//                        jdk              : '',
+//                        properties       : [],
+//                        reportBuildPolicy: 'ALWAYS',
+//                        results          : [[path: 'build/reports/allure-results']]
+//                ])
+//            }
         }
     }
 }
